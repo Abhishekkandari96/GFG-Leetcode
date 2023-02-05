c49eb37d7521299d21f63e2cecde2a128632c08e
Abhishek  kandari <117740642+Abhishekkandari96@users.noreply.github.com>
2023-02-05 20:01:23 +0530

    Added solution - LeetHub

---
+++ First element to occur k times - GFG/first-element-to-occur-k-times.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class GfG { 
+	public static void main (String[] args) throws IOException  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    int n = Integer.parseInt(inputLine[0]);
+		    int m = Integer.parseInt(inputLine[1]);
+		    int[] arr = new int[n];
+		    inputLine = br.readLine().trim().split(" ");
+		    for(int i=0; i<n; i++)arr[i] = Integer.parseInt(inputLine[i]);
+		    
+		    Solution obj = new Solution();
+		    System.out.println(obj.firstElementKTime(arr, n, m));
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public int firstElementKTime(int[] arr, int n, int k) { 
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i=0;i<n;i++){
+            hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
+            if(hm.get(arr[i])==k){
+                return arr[i];
+            }
+        }
+        return -1;
+        
+    } 
+}
