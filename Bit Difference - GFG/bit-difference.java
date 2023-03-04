2e482520bd1b7da28b1605e18ba80d27acbcdccf
Abhishek  kandari <117740642+Abhishekkandari96@users.noreply.github.com>
2023-03-04 13:03:03 +0530

    Added solution - LeetHub

---
+++ Bit Difference - GFG/bit-difference.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution{
+    
+    // Function to find number of bits needed to be flipped to convert A to B
+    public static int countBitsFlip(int a, int b){
+        
+        //step 1-xor
+        int xor=a^b;
+        // step 2- find no of set bit
+       int  count=0;
+       while(xor>0){
+           count++;
+           xor=xor&(xor-1);
+       }
+        return count;
+    }
+    
+    
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Main {
+    
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();//testcases
+		int m, n;
+		long ans = 0; // initialise ans to 0
+		while(t-->0) {
+		    
+		    //input m and n
+		    m = sc.nextInt();
+		    n = sc.nextInt();
+		    Solution obj = new Solution();
+		    System.out.println(obj.countBitsFlip(m, n));
+		}
+	}
+}
+
+
+// } Driver Code Ends
