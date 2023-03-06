26e7b64f06091c81f3aad65753982bfba4191ed6
Abhishek  kandari <117740642+Abhishekkandari96@users.noreply.github.com>
2023-03-06 12:10:01 +0530

    Added solution - LeetHub

---
+++ Minimum Operations - GFG/minimum-operations.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n = sc.nextInt();
+                    Solution ob = new Solution();
+                    System.out.println(ob.minOperation(n));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    public int minOperation(int n)
+    {
+        int ans=0;
+       while(n>=0){
+           if(n%2==1){
+               n--;
+               ans++;
+           }
+           else if(n%2==0){
+               n=n/2;
+                ans++;
+           }
+           if(n==0){
+               return ans;
+           }
+       }
+       return 0;
+    }
+}
