7e8079ddc2a35fb4fbae58d9b25f50df0ea7ad4c
Abhishek  kandari <117740642+Abhishekkandari96@users.noreply.github.com>
2023-03-09 11:05:29 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (11.12%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,9 +2,9 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         int n=nums.length;
         List<List<Integer>> subsets=new ArrayList<>();
-        for(int i=0;i<(1<<n);i++){     //determine tthe length of subset
+        for(int i=0;i<(1<<n);i++){     //determine tthe length of subset(in truth table it itterate verticle)
             List<Integer> set=new ArrayList<>();
-            for(int j=0;j<n;j++){         // check for each set bit
+            for(int j=0;j<n;j++){         // check for each set bit(in trueth table it itterate horizintally)
                  if ((i & (1 << j)) > 0){
                     set.add(nums[j]);
                 }
